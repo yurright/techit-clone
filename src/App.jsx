f09f2d8ca362ce20b0619be2b092f858d7b3c219
cryptojang <robert.crypto.jang@gmail.com>
2023-11-16 15:30:22 +0900

    layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,57 @@
 const App = () => {
-  return <div className="bg-red-100">Hello, React!</div>;
+  return (
+    <>
+      <div className="h-[42px] bg-purple-100">pop-up</div>
+      <header className="bg-red-100 h-16">header</header>
+      <div className="bg-orange-100">banner</div>
+      <section className="mt-20 bg-yellow-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-green-100 h-[64.5px]">KDT</div>
+        <div className="bg-blue-100 grid grid-cols-4 gap-6 justify-items-center">
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+          <div className="bg-purple-100 w-[290px] h-[416px]">Card</div>
+        </div>
+      </section>
+      <section className="mt-20 bg-yellow-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-green-100 h-[64.5px]">Schedule</div>
+        <div className="bg-blue-100 h-[280px]">KDT</div>
+      </section>
+      <div className=" mt-20 bg-yellow-100 h-[206px]">Alarm banner</div>
+      <section className="mt-20 bg-yellow-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-green-100 h-[64.5px]">Onboarding</div>
+        <div className="bg-blue-100 grid grid-cols-3 gap-6 justify-items-center">
+          <div className="bg-purple-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-purple-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-purple-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-purple-100 w-[397px] h-[316px]">Card</div>
+          <div className="bg-purple-100 w-[397px] h-[316px]">Card</div>
+        </div>
+      </section>
+      <section className="mt-20 bg-yellow-100 max-w-screen-xl mx-auto px-6">
+        <div className="bg-green-100 h-[64.5px]">Education</div>
+        <div className="bg-blue-100 grid grid-cols-2 gap-6 justify-items-center">
+          <div className="bg-purple-100 w-[604px] h-[310px]">Card</div>
+          <div className="bg-purple-100 w-[604px] h-[310px]">Card</div>
+          <div className="bg-purple-100 w-[604px] h-[310px]">Card</div>
+        </div>
+      </section>
+      <footer className="mt-40 max-w-screen-xl mx-auto bg-gray-100 h-[418px]">
+        footer
+      </footer>
+    </>
+  );
 };
 
 export default App;
